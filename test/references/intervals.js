346ef9e5d2fe98f128572b6d682dc4f0ec9252d8
Calin Seciu <calinseciu@gmail.com>
2015-06-05 13:26:39 +0300

    Improve 'Intervals' reference
    - detect negative task numbers as invalid, eg. '#-3'
    - detect '#0' as invalid (the API was returning all tasks)

--- test/references/intervals.js
+++ test/references/intervals.js
@@ -24,26 +24,28 @@ describe('references/intervals', function() {
 
     it('should parse correctly', function() {
         var refs = new Intervals.parse('Commit with task references\n\n' +
-        'Resolve #1, #2, no task #11abc and\n\ntask #11-3, task #9999999.');
+        'Resolve #1, #2, no task #11abc and\n\ntask #11-3, task #9999999\n' +
+        'and task #-2');
 
         assert.equal(refs[0].match, '#1');
         assert.equal(refs[1].task, 2);
         assert.equal(refs[2].match, '#11');
         assert.equal(refs[2].task, 11);
         assert.equal(refs[3].task, 9999999);
+        assert.equal(refs[4].task, -2);
     });
 
     it('should validate correctly using the API', function(done) {
         this.timeout(3000); // allow enough time
 
         var refs = new Intervals.parse('Validate issue references using APIs\n\n' +
-        'Resolve #1, #634, no task #11abc and\n\ntask #12312312324.', cfg);
+        'Resolve #1, #634, no task #11abc and\n\ntask #12312312324 with task #0.', cfg);
         var ct = 0;
         var isDone = function() {
             ct++;
             if (ct == refs.length) done();
         };
-        var results = [false, true, false];
+        var results = [false, true, false, false];
 
         results.forEach(function(val, idx) {
             refs[idx].isValid(function(err, valid) {
